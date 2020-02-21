f171d227bba09096cfc14ce866a595abfb2814c0
Stephen Puiszis <spuiszis@gmail.com>
2020-02-20 23:17:36 -0600

    Fix a lot of things

--- Homework6/nutrition.R
+++ Homework6/nutrition.R
@@ -15,16 +15,17 @@ summary(basic_model)
 anova(basic_model)
 
 # Check the Model
-residual_relationship = nutrition$woh ~ nutrition$age
+residual_relationship = basic_model$residuals ~ nutrition$age
 residual_model = lm(formula = residual_relationship)
 plot(
   residual_relationship,
   xlab = "Age",
-  ylab = "Fitted Values",
+  ylab = "Residuals",
   main = "Residual Plot",
   pch = 18
 )
 abline(residual_model, col=c("red"))
+summary(residual_model)
 
 hist(rstudent(basic_model), col = c("black"))
 qqnorm(rstudent(basic_model), col = c("black"), pch = 18)
@@ -50,16 +51,16 @@ qqnorm(rstudent(transformed_model), col = c("black"))
 abline(a = 0, b = 1, col = c("red"))
 
 ### Polynomial Model ###
-age_squared = nutrition$age ^ 2
-polynomial_relationship = nutrition$woh ~ nutrition$age + age_squared
+nutrition$age_squared = nutrition$age ^ 2
+polynomial_relationship = nutrition$woh ~ nutrition$age + nutrition$age_squared
 plot(
-  polynomial_relationship,
+  nutrition$woh ~ nutrition$age_squared,
   xlab = "Age (Months Squared)",
   ylab = "Weight to Height Squared",
   main = "Polynomial Relationship b/w W-to-H Ratio and Age",
   pch = 18
 )
-polynomial_model = lm(formula = nutrition$woh ~ nutrition$age + age_squared)
+polynomial_model = lm(formula = nutrition$woh ~ nutrition$age + nutrition$age_squared)
 lines(age_squared,fitted(polynomial_model), col = c("red"))
 summary(polynomial_model)
 anova(polynomial_model)
@@ -69,7 +70,7 @@ qqnorm(rstudent(polynomial_model), col = c("black"))
 abline(a = 0, b = 1, col = c("red"))
 
 # Dummy Model
-nutrition$under_7 = nutrition$age < 7
+nutrition$under_7 = as.numeric(nutrition$age < 7)
 nutrition$mixed = nutrition$under_7 * nutrition$age
 dummy_relationship = nutrition$woh ~
   nutrition$age +
@@ -77,7 +78,7 @@ dummy_relationship = nutrition$woh ~
   nutrition$mixed
 
 plot(
-  dummy_relationship,
+  nutrition$woh ~ nutrition$age,
   xlab = "Age",
   ylab = "Weight to Height",
   main = "Relationship b/w W-to-H Ratio and Age using Dummy Vars",
@@ -87,6 +88,17 @@ dummy_model = lm(dummy_relationship, data = nutrition)
 lines(nutrition$age, fitted(dummy_model), col = c("red"))
 summary(dummy_model)
 anova(dummy_model)
+
+# Check our Model
+dummy_residuals_relationship = dummy_model$residuals ~ nutrition$age
+plot(
+  dummy_residuals_relationship,
+  xlab = "Age",
+  ylab = "Residuals",
+  main = "Residuals of W-to-H Ratio and Age using Dummy Vars",
+  pch = 18
+)
+abline(lm(dummy_residuals_relationship), col = c("red"))
 
 hist(rstudent(polynomial_model), col = c("black"))
 qqnorm(rstudent(polynomial_model), col = c("black"))
@@ -98,4 +110,4 @@ model_aic = c(
   basic_model = extractAIC(basic_model)[2],
   dummy_model = extractAIC(dummy_model)[2]
 )
-
+print(model_aic)
